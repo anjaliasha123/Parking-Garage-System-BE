70cb77bab824b6683083b24a7446ad1886966a08
anjaliasha123 <anjaliasha95@gmail.com>
2024-01-16 15:08:19 -0600

    resolving cross origin issue

--- src/main/java/com/anjali/springboot/parkinggarage/controllers/UserController.java
+++ src/main/java/com/anjali/springboot/parkinggarage/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/parking/users")
+@CrossOrigin
 @AllArgsConstructor
 public class UserController {
     @Autowired
